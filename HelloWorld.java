6558010a086261648f687fdc74e56bb76ea8add7
mahesm4 <kalase.mahesh@gmail.com>
2017-06-20 01:33:17 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("!!!!! Hello, Jagdevi !!!!!!!!!!! ");
+        System.out.println(" ***** Final test for the master branch with auto SCM polling******  ");
     }
 
 }
